fc47f75c6f412542b115d653ad1c9b9175ef3976
Bruno Mendes <brpimendes@gmail.com>
2022-02-01 22:31:19 -0300

    Ajuste nas funcionalidades dos botoes redirecionando para uma nova pagina.

--- CloneFlix/js/main.js
+++ CloneFlix/js/main.js
@@ -1,23 +1,32 @@
 window.addEventListener("load", function () {
   const btnPlay = document.querySelector("#play");
   btnPlay.addEventListener("click", playVideo);
+
+  const btnInfo = document.querySelector("#info");
+  btnInfo.addEventListener("click", moreInfo);
 });
 
 function playVideo() {
-  const filmePrincipal = document.querySelector(".filme-principal");
+  // const filmePrincipal = document.querySelector(".filme-principal");
 
-  const modalPlayer = document.createElement("div");
-  modalPlayer.classList.add("modal-player");
+  // const modalPlayer = document.createElement("div");
+  // modalPlayer.classList.add("modal-player");
 
-  const player = document.createElement("video");
-  player.classList.add("player");
-  player.setAttribute("src", "https://youtu.be/I-t4PJKrb3A");
+  // const player = document.createElement("video");
+  // player.classList.add("player");
+  // player.setAttribute("src", "https://youtu.be/I-t4PJKrb3A");
 
-  const btnClose = document.createElement("span");
-  btnClose.append(document.createTextNode("X"));
+  // const btnClose = document.createElement("span");
+  // btnClose.append(document.createTextNode("X"));
 
-  player.appendChild(btnClose);
-  modalPlayer.appendChild(player);
+  // player.appendChild(btnClose);
+  // modalPlayer.appendChild(player);
 
-  filmePrincipal.appendChild(modalPlayer);
+  // filmePrincipal.appendChild(modalPlayer);
+
+  location.href = "play.html";
+}
+
+function moreInfo() {
+  location.href = "moreInfo.html";
 }
